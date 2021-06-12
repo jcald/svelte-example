c311380a0e618237e0730c3bddf821979d1fce89
José Calderón Celis <josecalderoncelis@gmail.com>
2021-06-11 23:50:54 -0500

    Revert "Cambios de estilo"
    This reverts commit a9f5fe582c6b039a891ab1954c9bab90aeb20839.

--- src/App.svelte
+++ src/App.svelte
@@ -16,21 +16,4 @@
 	{count}
 </h2>
 
-<style>
-h2 {
-	font-size: 4em;
-	color:rgb(255, 30, 0);
-}
-button {
-    background-color: rgb(241, 198, 3);
-    border-radius: 4px;
-    font-size: 4em;
-    padding: 0.2em 0.5em;
-    border-top: 7px solid rgba(255,255,255,0.5);
-    border-left: 7px solid rgba(255,255,255,0.5);
-    border-right: 7px solid rgba(0,0,0,0.2);
-    border-bottom: 7px solid rgba(0,0,0,0.2);
-    color: #f55;
-  }
 
-</style>
